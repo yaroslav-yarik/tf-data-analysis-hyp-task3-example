5eabd66d3e30790027041b7995e8fea4f6c752b4
yaroslav-yarik <86802498+yaroslav-yarik@users.noreply.github.com>
2023-04-13 22:25:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+chat_id = 561049466
+
+def solution(sample: np.ndarray) -> bool:
+    _, p_v = ttest_1samp(sample, 300)
+    return p_v < 0.04:
